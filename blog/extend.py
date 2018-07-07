df09ad3c4ea1c7f7413b47abbd83f3011083a108
xutian <2463772128@qq.com>
2018-07-07 22:17:06 +0800

    2018.07.07.01

--- blog/extend.py
+++ blog/extend.py
@@ -12,3 +12,16 @@ def get_category(category_slug):
             children_category_dict_list.append(get_category(children_category.slug))
         node.update({"children": children_category_dict_list})
     return node
+
+def get_category_beta(category_slug):
+    node = {}
+    category_object = Category.objects.get(slug=category_slug)
+    node = {"id": category_object.slug, "text": category_object.name}
+    children_category_set = category_object.category_set.all()
+    if children_category_set:
+        children_category_dict_list = []
+        for children_category in children_category_set:
+            print(children_category.slug)
+            children_category_dict_list.append(get_category(children_category.slug))
+        node.update({"children": children_category_dict_list})
+    return node
