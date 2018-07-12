51b4c6f0da6420d71ee8be332a20fef8eb35992b
Dexutian <2463772128@qq.com>
2018-07-12 20:36:27 +0800

    2018.07.12

--- stock/urls.py
+++ stock/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('index/', views.index, name='stock_index'),
     path('get_menu_data/', views.get_menu_data, name='get_menu_data'),
     #股票名称
-    path('name/', views.name, name='name'),
+    path('name_file/', views.name_file, name='name_file'),
     path('upload_name_file', views.upload_name_file, name='upload_name_file'),
-    path('update_name/', views.update_name, name='update_name')
+    path('update_name/', views.update_name, name='update_name'),
+    path('name_data/', views.name_data, name='name_data'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-import json, glob, os
+import json, glob, os, logging
+from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from lib.extend import get_stock_menu
 from lib.manage_stock_name import get_excel_data_dict, update_name_use_dict
 from stock.models import Menu,Name
+
+# 生成logger
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+handler = logging.FileHandler(os.path.join(settings.MEDIA_ROOT, 'log', 'stock.log'))
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # Create your views here.
 def index(request):
@@ -34,25 +44,16 @@ def get_menu_data(request):
         data_json.append(get_stock_menu(menu.slug))
     return HttpResponse(json.dumps(data_json), content_type='application/json')
 
-def name(request):
+def name_file(request):
 	'''
-	股票名称首页
+	股票名称数据文件
 	:param request:
 	:return:
 	'''
 	files = []
-	for file in glob.glob(os.path.join(settings.MEDIA_ROOT, 'stockname', '*')):
+	for file in glob.glob(os.path.join(settings.MEDIA_ROOT, 'stockname', '*.xls*')):
 		files.append(file)
-	names = Name.objects.all()
-	paginator = Paginator(names, 20)
-	page = request.GET.get('page')
-	try:
-		name_list = paginator.page(page)
-	except PageNotAnInteger:
-		name_list = paginator.page(1)
-	except EmptyPage:
-		name_list = paginator.page(paginator.num_pages)
-	return render(request, 'stock/stock_name.html',{'files':files, 'stocknames':name_list})
+	return render(request, 'stock/stock_name_file.html', {'files':files})
 
 def upload_name_file(request):
 	'''
@@ -60,19 +61,28 @@ def upload_name_file(request):
 	:param request:
 	:return:
 	'''
+
 	# 请求方法为POST时，进行处理
 	if request.method == "POST":
 		# 获取上传的文件，如果没有文件，则默认为None
 		myFile =request.FILES.get("myfile", None)
 		if not myFile:
+			logger.info("no files for upload!")
 			return HttpResponse("no files for upload!")
-		# 打开特定的文件进行二进制的写操作
-		destination = open(os.path.join(settings.MEDIA_ROOT, 'stockname', myFile.name),'wb+')
-		# 分块写入文件
-		for chunk in myFile.chunks():
-			destination.write(chunk)
-		destination.close()
-		return HttpResponseRedirect("/stock/name/")
+		else:
+			file_suffix = myFile.name.split(".")[-1]
+			if file_suffix not in ["xls", "xlsx"]:
+				logger.info("uploading file type is not supported!")
+				return HttpResponse("uploading file type is not supported!")
+			else:
+				# 打开特定的文件进行二进制的写操作
+				destination = open(os.path.join(settings.MEDIA_ROOT, 'stockname', myFile.name),'wb+')
+				# 分块写入文件
+				for chunk in myFile.chunks():
+					destination.write(chunk)
+				destination.close()
+				logger.info(myFile.name + "---上传成功！")
+				return HttpResponseRedirect("/stock/name_file/")
 
 @csrf_exempt
 def update_name(request):
@@ -83,6 +93,35 @@ def update_name(request):
 	'''
 	if request.method=="POST":
 		file_excel=request.POST.get("k1")
-	data=get_excel_data_dict(file_excel=file_excel, col_name_index=0, by_index=0)
-	insert_num = update_name_use_dict(data)
-	return HttpResponse(str(insert_num))
+		action = request.POST.get("action")
+	if action == "更新数据":
+		data=get_excel_data_dict(file_excel=file_excel, col_name_index=0, by_index=0)
+		insert_num = update_name_use_dict(data)
+		msg = "成功更新" + str(insert_num) + "数据！"
+		logger.info(msg)
+	if action == "删除文件":
+		try:
+			os.remove(file_excel)
+			logger.info(file_excel + '---删除成功')
+			msg = "文件删除成功！"
+		except Exception as e:
+			logger.info(file_excel + '---删除错误，错误信息：' + e)
+			msg = "文件删除错误！"
+	return HttpResponse(msg)
+
+def name_data(request):
+	'''
+	股票名称数据页
+	:param request:
+	:return:
+	'''
+	names = Name.objects.all()
+	paginator = Paginator(names, 20)
+	page = request.GET.get('page')
+	try:
+		name_list = paginator.page(page)
+	except PageNotAnInteger:
+		name_list = paginator.page(1)
+	except EmptyPage:
+		name_list = paginator.page(paginator.num_pages)
+	return render(request, 'stock/stock_name_data.html',{ 'stocknames':name_list})
