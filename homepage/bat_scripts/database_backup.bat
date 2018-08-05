c55ffb2231c4eff23a38fdbb50a5f13522f02d3b
xutian <2463772128@qq.com>
2018-08-05 19:54:15 +0800

    v0.10 2018.08.05

--- homepage/bat_scripts/database_backup.bat
+++
@@ -1,9 +0,0 @@
-@rem 初始化系统数据库
-@echo off
-cd /d %~d0\pycode\env_python3.6.5\Scripts
-call activate.bat
-cd /d %~d0\pycode\homepage
-python manage.py dumpdata admin --format=json > requirements\admin.json
-python manage.py dumpdata auth --format=json > requirements\auth.json
-python manage.py dumpdata info --format=json > requirements\info.json
-pause
--- homepage/bat_scripts/test.bat
+++
@@ -1,6 +0,0 @@
-@rem 启动测试程序
-@echo off
-cd /d %~d0\pycode\env_python3.6.5\Scripts
-call activate.bat
-cd /d %~d0\pycode\homepage
-python manage.py test
